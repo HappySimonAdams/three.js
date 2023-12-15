de45a51ec92f8a2c8a89897477f44e87f1e041a9
simon <simonadams1123@163.com>
2023-12-15 15:59:48 +0800

    feat: add readPixelForMRT()

--- samples/common/picking.js
+++ samples/common/picking.js
@@ -5,6 +5,8 @@ import {
 	Plane,
 	WebGLRenderTarget,
 	Color,
+	FloatType,
+	UnsignedByteType,
 } from '../../src/Three.js';
 
 const _raycaster = new Raycaster();
@@ -48,7 +50,6 @@ export function rayPickPosition( options ) {
 
 }
 
-// GPU拾取。pickSize为奇数，表示拾取像素块的尺寸
 export function readPixel( options ) {
 
 	const { camera, renderer, selectObject, x, y, pickSize = 3, pickRenderTarget = undefined } = options;
@@ -69,9 +70,9 @@ export function readPixel( options ) {
 	// renderer.setClearColor(_oldClearColor, _oldClearAlpha);
 	// renderer.setRenderTarget(null);
 	// camera.clearViewOffset();
-	// const pixes = new Uint8Array(pickSize * pickSize * 4);
-	// renderer.readRenderTargetPixels(rt, 0, 0, pickSize, pickSize, pixes);
-	// return pixes;
+	// const pixels = new Uint8Array(pickSize * pickSize * 4);
+	// renderer.readRenderTargetPixels(rt, 0, 0, pickSize, pickSize, pixels);
+	// return pixels;
 
 	// 方式二：renderTarget包含整个屏幕范围
 	rt.setSize( window.innerWidth, window.innerHeight );
@@ -80,19 +81,54 @@ export function readPixel( options ) {
 	renderer.render( selectObject, camera );
 	renderer.setClearColor( _oldClearColor, _oldClearAlpha );
 	renderer.setRenderTarget( null );
-	const pixes = new Uint8Array( pickSize * pickSize * 4 );
+	const pixels = new Uint8Array( pickSize * pickSize * 4 );
 	// 左下角为坐标原点，Y坐标要反转坐标系
-	renderer.readRenderTargetPixels( rt, x - bias, window.innerHeight - y - bias, pickSize, pickSize, pixes );
-	return pixes;
+	renderer.readRenderTargetPixels( rt, x - bias, window.innerHeight - y - bias, pickSize, pickSize, pixels );
+	return pixels;
+
+}
+
+// 读取 multipleRenderTargets 像素值
+export function readPixelForMRT( options ) {
+
+	const { camera, renderer, selectObject, x, y, pickSize = 3, pickRenderTarget, activeTextureIndex = 0 } = options;
+	// const bias = ( pickSize - 1 ) / 2;
+	const bias = pickSize / 2;
+
+	renderer.getClearColor( _oldClearColor );
+	_oldClearAlpha = renderer.getClearAlpha();
+
+	pickRenderTarget.setSize( window.innerWidth, window.innerHeight );
+	renderer.setRenderTarget( pickRenderTarget );
+	renderer.setClearColor( 0x000000, 0 );
+	renderer.render( selectObject, camera );
+	renderer.setClearColor( _oldClearColor, _oldClearAlpha );
+	renderer.setRenderTarget( null );
+
+	const texture = pickRenderTarget.texture[ activeTextureIndex ];
+	let pixels;
+	if ( texture.type === FloatType ) {
+
+		pixels = new Float32Array( pickSize * pickSize * 4 );
+
+	} else if ( texture.type === UnsignedByteType ) {
+
+		pixels = new Uint8Array( pickSize * pickSize * 4 );
+
+	}
+
+	// 左下角为坐标原点，Y坐标要反转坐标系
+	renderer.readMultipleRenderTargetPixels( pickRenderTarget, x - bias, window.innerHeight - y - bias, pickSize, pickSize, pixels, activeTextureIndex );
+	return pixels;
 
 }
 
 export function gpuPickId( options ) {
 
-	const pixes = readPixel( options );
+	const pixels = readPixel( options );
 
 	// 将4位数转成1位数，即为pickId
-	const int32Array = new Int32Array( pixes.buffer );
+	const int32Array = new Int32Array( pixels.buffer );
 
 	let pickId = 0;
 	let pickIdColor = [ 0, 0, 0, 0 ];
@@ -103,7 +139,7 @@ export function gpuPickId( options ) {
 		// 拾取时，clearColor为: (0, 0, 0, 0)
 		if ( pickId !== 0 ) {
 
-			pickIdColor = [ pixes[ i * 4 ], pixes[ i * 4 + 1 ], pixes[ i * 4 + 2 ], pixes[ i * 4 + 3 ] ];
+			pickIdColor = [ pixels[ i * 4 ], pixels[ i * 4 + 1 ], pixels[ i * 4 + 2 ], pixels[ i * 4 + 3 ] ];
 			break;
 
 		}
